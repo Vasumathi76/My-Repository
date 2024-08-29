8fc9ddd1e3a9ffa1ccee331a5b004966b9980329
Vasumathi76 <vasumathik003@gmail.com>
2024-08-29 01:32:06 -0700

    JAVA FILE
    ASSIGNMENT

---
+++ Assignment.java
@@ -0,0 +1,11 @@
+public class Assignment
+{
+public static void main(String args[])
+{
+System.out.println("Name:Vasumathi");
+System.out.println("Reg No:731222205046");
+System.out.println("Branch:B.tech Information Technology");
+System.out.println("Year of Studying:2nd year");
+System.out.println("College Name:Jkk Munirajah College Of Technology");
+}
+}
